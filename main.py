aed51809fea73b4c4436a2e1ee0e68cd0fd16bbe
Little Von <122691411@qq.com>
2018-01-12 18:05:48 +0800

    改改改 bug

--- main.py
+++ main.py
@@ -14,15 +14,9 @@ search_url = 'https://www.baidu.com/'
 # 题目的来源url
 source_url = 'http://htpmsg.jiecaojingxuan.com/msg/current'
 
-# 三个浏览器的搜索框和搜索按钮
-webTextEdit1 = None
-button1 = None
-
-webTextEdit2 = None
-button2 = None
-
-webTextEdit3 = None
-button3 = None
+# 浏览器的搜索框和搜索按钮
+webTextEdit = None
+button = None
 
 # 问题集合
 questions = []
@@ -37,50 +31,28 @@ headers = {
 
 # 打开百度
 def openWeb():
-    global webTextEdit1, button1, webTextEdit2, button2, webTextEdit3, button3
-    driver1 = webdriver.Chrome()         # 浏览器
-    driver1.get(search_url)
-    webTextEdit1 = driver1.find_element_by_xpath('//*[@id="kw"]')
-    button1 = driver1.find_element_by_xpath('//*[@id="su"]')
-
-    driver2 = webdriver.Chrome()         # 浏览器
-    driver2.get(search_url)
-    webTextEdit2 = driver2.find_element_by_xpath('//*[@id="kw"]')
-    button2 = driver2.find_element_by_xpath('//*[@id="su"]')
-
-    driver3 = webdriver.Chrome()         # 浏览器
-    driver3.get(search_url)
-    webTextEdit3 = driver3.find_element_by_xpath('//*[@id="kw"]')
-    button3 = driver3.find_element_by_xpath('//*[@id="su"]')
+    global webTextEdit, button
+    driver = webdriver.Chrome()         # 浏览器
+    driver.get(search_url)
+    webTextEdit = driver.find_element_by_xpath('//*[@id="kw"]')
+    button = driver.find_element_by_xpath('//*[@id="su"]')
 
 
 # 搜索函数
-def searchAnswer(query, postfix):
+def searchAnswer(query):
     '''
     Args: 
         query:   搜索的题目字符串
-        postfix: 后缀, 选项的list表
 
     Returns: 
         None
     '''
-    global webTextEdit1, button1, webTextEdit2, button2, webTextEdit3, button3
+    global webTextEdit, button
     try:
         query = query[query.find('.') + 1: query.find('?')]
-        # 搜索题目+第1个选项
-        webTextEdit1.clear()
-        webTextEdit1.send_keys(query + ' ' + postfix[0])
-        button1.click()
-
-        # 搜索题目+第2个选项
-        webTextEdit2.clear()
-        webTextEdit2.send_keys(query + ' ' + postfix[1])
-        button2.click()
-
-        # 搜索题目+第3个选项
-        webTextEdit3.clear()
-        webTextEdit3.send_keys(query + ' ' + postfix[2])
-        button3.click()
+        webTextEdit.clear()
+        webTextEdit.send_keys(query)
+        button.click()
     except:
         print('本次自动搜索出错，请复制粘贴搜索。')
 
@@ -127,12 +99,13 @@ def getInfo():
                 # 题目
                 if question not in questions:
                     questions.append(question)
-                    print(question)     # 显示题目
-                    print(*option)      # 显示选项
+                    print(question)              # 显示题目
+                    print("选项: ")
+                    print(" ".join(option))      # 显示选项
 
                     # 开线程去自动搜索
                     threading.Thread(target=searchAnswer,
-                                     args=(question, option)).start()
+                                     args=(question, )).start()
 
                 # 答案, 这是题目之后才会抓到的答案包, 不能提前得知
                 if correct_option != None:
@@ -142,18 +115,19 @@ def getInfo():
                         options.append(option)
                         print('正确答案: ' + str(correct_answer))
 
-                        save(question, option, correct_option)  # 保存到文件
+                        threading.Thread(target=save, args=(
+                            question, option, correct_option)).start()  # 保存到文件
 
                         print('\nWaiting for new question')
 
         except:
-            print('￣□￣｜｜突然发生点小错误')
+            pass
     except:
-        return '网络异常!!!!'
+        pass
 
 
 def init():
-    # 线程打开三个网页
+    # 线程打开网页
     threading.Thread(target=openWeb).start()
 
     # 在保存的文件中写入时间
@@ -177,4 +151,17 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    print('Waiting for question')
+
+    threading.Thread(target=openWeb).start()
+
+    time.sleep(8)
+
+    qu = '公认的世界上第一辆现代汽车是谁造出来并申请专利的？'
+    op = ["尼考罗斯·奥托", "卡尔·本茨", "威廉姆斯·戴姆勒"]
+    print(qu)
+    print(" ".join(op))
+    threading.Thread(target=searchAnswer,
+                                     args=(qu, )).start()
+    time.sleep(20)
